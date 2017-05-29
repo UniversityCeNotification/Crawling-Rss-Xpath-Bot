40d998c0f119147b099e1a2a11ef73b08aa39ba0
pleycpl <h4rvey@protonmail.com>
2017-05-29 12:26:36 +0000

    outputs colorized

--- main.py
+++ main.py
@@ -4,14 +4,23 @@ import feedparser
 from pymongo import MongoClient
 from dotenv import DotEnv
 dotenv = DotEnv('.env')
-
 client = MongoClient(dotenv.get('MongoDbUri', 'mongodb://localhost:27017'))
 db = client[dotenv.get('MongoDbName', 'rsscrawler')] # which database
 crawlers = db.crawlers  # which collection
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 def crawl(url):
     if url == '' or not url.startswith('http'):
-        print('Error: url can not be empty string or url should startwith http')
+        print(bcolors.FAIL + 'Error: url can not be empty string or url should startwith http' + bcolors.ENDC)
         return
     d = feedparser.parse(url)
     for entry in d['entries']:
@@ -25,13 +34,17 @@ def crawl(url):
             print('New Link crawled')
             crawlers.insert_one(data)
 
-    print('[-] Crawling Site Finished')
+    print(bcolors.OKGREEN + '[-] Crawling Site Finished' + bcolors.OKGREEN)
 
 if __name__ == '__main__':
+    print(bcolors.OKBLUE + '\n[*] Program Started' + bcolors.ENDC)
     Files = ['sites/'+ File for File in os.listdir('sites') if File.endswith('.json')]
     print(Files)
     for File in Files:
         with open(File) as FileJsonData:
             d = json.load(FileJsonData)
-            print('[+] Crawling, SiteName: ' + d.get('Site', 'Nope') + ' | SiteLink: ' + d.get('SiteLink', 'Nope') + ' | SiteRssLink: ' + d.get('SiteRssLink', 'Nope'))
+            print(bcolors.OKGREEN + '[+] Crawling Site:\n'+
+                ' SiteName: ' + d.get('Site', 'Nope') +
+                ' | SiteLink: ' + d.get('SiteLink', 'Nope') +
+                ' | SiteRssLink: ' + d.get('SiteRssLink', 'Nope') + bcolors.ENDC)
             crawl(d.get('SiteRssLink', ''))
