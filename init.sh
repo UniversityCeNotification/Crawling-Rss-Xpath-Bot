d102d50000fe027e7d2b8f9ef142b32501957d4a
pleycpl <h4rvey@protonmail.com>
2017-05-29 14:27:18 +0000

    controlling parameter

--- init.sh
+++ init.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Checking parameter
+if [ "$1" = "" ]
+then
+    time=60
+else
+    time=$1
+fi
+
+echo $time "second"
+
+# Checking .env file
 read -p 'There is .env file in crawler?(y/n) : ' status
 if [[ "$status" = "n" ]]; then
   read -p "MongoDbUrl:" url
@@ -8,12 +19,13 @@ if [[ "$status" = "n" ]]; then
   echo "MongoDbName='$name'" >> .env
 fi
 
+# Crawling start
 virtualenv venv
 source venv/bin/activate
 pip install -r requirements.txt
 while true
 do
   python main.py
-  sleep 60
+  sleep $time
 done
 deactivate
