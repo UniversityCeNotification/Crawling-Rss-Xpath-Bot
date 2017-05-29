ffc9f51c3284db89cfab085620a0ef1dbf3ed242
pleycpl <h4rvey@protonmail.com>
2017-05-29 12:16:27 +0000

    added init.sh file

---
+++ init.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+read -p 'There is .env file in crawler?(y/n) : ' status
+if [[ "$status" = "n" ]]; then
+  read -p "MongoDbUrl:" url
+  read -p "MongoDbName:" name
+  echo "MongoDbUri='$url'" > .env
+  echo "MongoDbName='$name'" >> .env
+fi
+
+virtualenv venv
+source venv/bin/activate
+pip install -r requirements.txt
+python main.py
+deactivate
