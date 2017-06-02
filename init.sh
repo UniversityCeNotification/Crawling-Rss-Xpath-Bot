80b73e45f6763b21433a161e3c88ea8c65dbd97a
pleycpl <h4rvey@protonmail.com>
2017-06-02 15:07:35 +0000

    added started bot script

--- init.sh
+++ init.sh
@@ -15,11 +15,23 @@ read -p 'There is .env file in crawler?(y/n) : ' status
 if [[ "$status" = "n" ]]; then
   read -p "MongoDbUrl:" url
   read -p "MongoDbName:" name
+  read -p "TelegramToken:" token
   echo "MongoDbUri='$url'" > .env
   echo "MongoDbName='$name'" >> .env
+  echo "TelegramToken='$token'" >> .env
 fi
 
+# Started Bot
+cd bot
+virtualenv venv
+source venv/bin/activate
+pip install -r requirements.txt
+python main.py &
+deactivate
+cd -
+
 # Crawling start
+cd crawler
 virtualenv venv
 source venv/bin/activate
 pip install -r requirements.txt
