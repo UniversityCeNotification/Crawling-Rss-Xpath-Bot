ccceb7c1f9d22da85b3f90b73d99514182ac5fec
pleycpl <h4rvey@protonmail.com>
2017-06-23 14:19:41 +0000

    checking init field, after that change status field

--- crawler/main.py
+++ crawler/main.py
@@ -23,7 +23,7 @@ def write_file(data):
     """ writing data to file """
     return data
 
-def insert_mongo_db(data, siteurl):
+def insert_mongo_db(isinit, data, siteurl):
     """ inserting data to mongodb """
     if not data == {}:
         result = CRAWLERS.find_one({'link': data['link']})
@@ -31,9 +31,11 @@ def insert_mongo_db(data, siteurl):
             print('New Link crawled and inserted mongodb')
             print(data)
             data['siteurl'] = siteurl
+            if isinit == 'True':
+                data['status'] = 'old'
             CRAWLERS.insert_one(data)
 
-def crawl_with_xpath(site_link, list_xpath, url_xpath, title_xpath, pubdate_xpath):
+def crawl_with_xpath(isinit, site_link, list_xpath, url_xpath, title_xpath, pubdate_xpath):
     """ crawling url with xpaths """
     page = requests.get(site_link)
     tree = html.fromstring(page.content)
@@ -48,7 +50,7 @@ def crawl_with_xpath(site_link, list_xpath, url_xpath, title_xpath, pubdate_xpat
             'pubdate': pubdate,
             'status': 'new'
         }
-        insert_mongo_db(data, site_link)
+        insert_mongo_db(isinit, data, site_link)
 
 def get_value_in_dict(d, *args):
     for arg in args:
@@ -66,7 +68,7 @@ def general_rss_content_parse(entry):
         'status': 'new'
     }
 
-def crawl_with_rss(rssurl, siteurl):
+def crawl_with_rss(isinit, rssurl, siteurl):
     """ crawling with rss link """
     if rssurl == '' or not rssurl.startswith('http'):
         print(Bcolors.FAIL('Error: url can not be empty string or url should startwith http'))
@@ -74,7 +76,7 @@ def crawl_with_rss(rssurl, siteurl):
     rss = feedparser.parse(rssurl)
     for entry in rss['entries']:
         data = general_rss_content_parse(entry)
-        insert_mongo_db(data, siteurl)
+        insert_mongo_db(isinit, data, siteurl)
 
 if __name__ == '__main__':
     print(Bcolors.OKBLUEFUNC('\n[*] Program Started'))
@@ -87,8 +89,8 @@ if __name__ == '__main__':
             print(site)
             print(Bcolors.OKGREENFUNC('[+] Crawling Site:\n Name: {} | Link: {} | RssLink: {}'.format(site['SiteName'], site['SiteLink'], site['SiteRssLink'])))
             if not site['SiteRssLink'] == '':
-                crawl_with_rss(site['SiteRssLink'], site['SiteLink'])
+                crawl_with_rss(site['Init'], site['SiteRssLink'], site['SiteLink'])
                 print(Bcolors.OKGREENFUNC('[-] Crawling Rss Site Finished'))
             else:
-                crawl_with_xpath(site['SiteLink'], site['Xpath']['ListXpath'], site['Xpath']['UrlXpath'], site['Xpath']['TitleXpath'], site['Xpath']['PubDateXpath'])
+                crawl_with_xpath(site['Init'], site['SiteLink'], site['Xpath']['ListXpath'], site['Xpath']['UrlXpath'], site['Xpath']['TitleXpath'], site['Xpath']['PubDateXpath'])
                 print(Bcolors.OKGREENFUNC('[-] Crawling Xpath Site Finished'))
